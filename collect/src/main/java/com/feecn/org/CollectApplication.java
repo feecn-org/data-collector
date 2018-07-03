49e5c78f7f8bf7ee18d0866b1d0523d025df66b8
pczhangyu <13717713687@163.com>
2018-07-03 15:28:44 +0800

    init

---
+++ collect/src/main/java/com/feecn/org/CollectApplication.java
@@ -0,0 +1,15 @@
+package com.feecn.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pczhangyu
+ */
+@SpringBootApplication
+public class CollectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectApplication.class, args);
+	}
+}
